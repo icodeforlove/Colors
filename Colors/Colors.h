aae9cf4cc4153568b5634434e209e3967777e4d2
Chad Scira <chadvscira@gmail.com>
2015-03-07 16:00:41 +0700

    Initial Commit

---
+++ Colors/Colors.h
@@ -0,0 +1,19 @@
+//
+//  Colors.h
+//  Colors
+//
+//  Created by Chad Scira on 3/7/15.
+//  Copyright (c) 2015 Chad Scira. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Colors.
+FOUNDATION_EXPORT double ColorsVersionNumber;
+
+//! Project version string for Colors.
+FOUNDATION_EXPORT const unsigned char ColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colors/PublicHeader.h>
+
+
